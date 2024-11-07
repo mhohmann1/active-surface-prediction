9567a317500493da5defa4677123acde7723a25b
mhohmann1 <michael.hohmann@hsu-hh.de>
2024-11-07 16:20:10 +0100

    found bug

--- utils/helpers.py
+++ utils/helpers.py
@@ -5,16 +5,14 @@ import numpy as np
 def save_best_model(avg_test_loss, best_loss, epoch, path, model, optimizer):
     if avg_test_loss < best_loss:
         best_loss = avg_test_loss
-
         print(20 * "#")
         print("Best model at epoch: %i, with loss: %.4f" % (epoch, best_loss))
         print(20 * "#")
-
         torch.save({"epoch": epoch,
                     "model_state_dict": model.state_dict(),
                     "optimizer_state_dict": optimizer.state_dict(),
-                    "loss": best_loss}, path)
-
+                    "loss": best_loss,
+                    }, path)
     return best_loss
 
 def load_model(path, model, optimizer, eval=True):

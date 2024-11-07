c2df7e5b64999a40c59c2a0ad4f756a40aeea03c
mhohmann1 <michael.hohmann@hsu-hh.de>
2024-11-07 13:22:53 +0100

    changed args

--- args.py
+++ args.py
@@ -5,7 +5,6 @@ parser.add_argument("--path", default="data/Geometrien", help="Path of dataset."
 parser.add_argument("--epochs", default=1000, help="Number of epochs.", type=int)
 parser.add_argument("--batch_size", default=16, help="Number of batch size.", type=int)
 parser.add_argument("--workers", default=0, help="Number of workers.", type=int)
-# parser.add_argument("--learning_rate", default=1e-3, help="Size of learning rate.", type=int)
 parser.add_argument("--seed", default=42, help="Seed for reproducibility.", type=int)
 parser.add_argument("--img_size_w", default=256, help="Width of height map.", type=int)
 parser.add_argument("--img_size_h", default=128, help="Height of height map.", type=int)

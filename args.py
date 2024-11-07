cacf1861699bc3861204a20249a67c30886e3509
mhohmann1 <michael.hohmann@hsu-hh.de>
2024-11-07 15:55:28 +0100

    added inject noise arg

--- args.py
+++ args.py
@@ -9,4 +9,5 @@ parser.add_argument("--seed", default=42, help="Seed for reproducibility.", type
 parser.add_argument("--img_size_w", default=256, help="Width of height map.", type=int)
 parser.add_argument("--img_size_h", default=128, help="Height of height map.", type=int)
 parser.add_argument("--model", default="encoderdecoder", help="Parse 'encoderdecoder', 'unet' or 'pix2pix'.", type=str)
+parser.add_argument("--inject_noise", default=0.0, help="Level of injected noise (0.01 for 1% noise).", type=float)
 args = parser.parse_args()
